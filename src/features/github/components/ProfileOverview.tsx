d69e7f3096aa57b23c6c98e04b5f1b307701a1b7
sameer shah <shahkhanpc8925@gmail.com>
2026-05-02 20:38:29 +0000

    Modified types of

--- src/features/github/components/ProfileOverview.tsx
+++ src/features/github/components/ProfileOverview.tsx
@@ -1,36 +1,11 @@
-import { useState, useEffect } from "react"
 import { useGithubStore } from "../../../store/useGithubStore";
 
-interface UserResponse {
-    name: string,
-    bio: string,
-    avatar_url: string;
-    public_repos: number,
-    public_gists: number,
-    followers: number,
-
-}
-
 export function ProfileOverview() {
-    const [userData, setUserData] = useState<UserResponse | null>(null);
     const { user, loading, error } = useGithubStore();
 
-    useEffect(() => {
-        if (!user) return;
-        const data: UserResponse = {
-            name: user.name ?? "",
-            bio: user.bio ?? "",
-            avatar_url: user.avatar_url ?? "",
-            public_repos: user.public_repos ?? 0,
-            public_gists: user.public_gists ?? 0,
-            followers: user.followers ?? 0,
-        };
-        setUserData(data);
-    }, [user]);
-
-    if (loading.user) return <div>Loading...</div>
-    if (error) return <div>{error}</div>
-    if (!userData) return null;
+    if (loading.user) return <div>Loading...</div>;
+    if (error) return <div>{error}</div>;
+    if (!user) return null;
 
     return (
         <div className="p-5 bg-bg border border-border rounded-2xl shadow-sm hover:shadow-lg transition w-full">
@@ -41,22 +16,24 @@ export function ProfileOverview() {
 
             <div className="flex items-center gap-4 mb-5">
                 <img
-                    src={userData.avatar_url}
+                    src={user.avatar_url}
                     className="w-14 h-14 rounded-full border"
                 />
 
                 <div>
-                    <h4 className="font-semibold text-lg">{userData.name}</h4>
+                    <h4 className="font-semibold text-lg">
+                        {user.name || user.login}
+                    </h4>
                     <p className="text-sm text-text-main/70">
-                        {userData.bio}
+                        {user.bio || "No bio available"}
                     </p>
                 </div>
             </div>
 
             <div className="grid grid-cols-3 gap-3">
-                <Stat title="Repos" value={userData.public_repos} />
-                <Stat title="Followers" value={userData.followers} />
-                <Stat title="Gists" value={userData.public_gists} />
+                <Stat title="Repos" value={user.public_repos} />
+                <Stat title="Followers" value={user.followers} />
+                <Stat title="Gists" value={user.public_gists} />
             </div>
         </div>
     );
